e37018a1ce9a11053f5984428f94982296f5c3cb
Adriano Rossetto <53878258+DexterFLN@users.noreply.github.com>
2024-07-04 16:46:27 -0300

    test: Ajuste da organização e espaçamento dos testes executados.

--- src/test/java/br/com/controle/certo/application/usecase/expense/GetExpenseByIdUseCaseTest.java
+++ src/test/java/br/com/controle/certo/application/usecase/expense/GetExpenseByIdUseCaseTest.java
@@ -1,11 +1,8 @@
 package br.com.controle.certo.application.usecase.expense;
-
 import br.com.controle.certo.infrastructure.entrypoint.model.response.ResponseCategory;
 import br.com.controle.certo.infrastructure.entrypoint.model.response.ResponseExpensive;
-import br.com.controle.certo.application.usecase.expense.GetExpenseByIdUseCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -15,16 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 public class GetExpenseByIdUseCaseTest {
-
     @Mock
     private GetExpenseByIdUseCase getExpenseByIdUseCase;
-
     @Test
     void testGetExpenseById() {
-
         ResponseExpensive expectedResponse = ResponseExpensive.builder()
                 .idExpense(1)
                 .uuidDespesa("uuid123")
@@ -35,16 +28,12 @@ public class GetExpenseByIdUseCaseTest {
                 .expenseType("Type")
                 .dhCreate(LocalDateTime.now())
                 .dhUpdate(LocalDateTime.now())
-                .responseCategory(new ResponseCategory())  // Suponha que ResponseCategory também tenha um builder ou um construtor padrão
+                .responseCategory(new ResponseCategory())
                 .build();
-
-
         when(getExpenseByIdUseCase.getExpenseById(anyInt(), anyString()))
                 .thenReturn(expectedResponse);
 
-
         ResponseExpensive actualResponse = getExpenseByIdUseCase.getExpenseById(1, "userDocument");
-
 
         assertEquals(expectedResponse, actualResponse);
     }
