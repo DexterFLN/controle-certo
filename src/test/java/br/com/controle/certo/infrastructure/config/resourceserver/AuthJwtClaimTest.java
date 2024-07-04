a7a8e3ca40189b82d5e2f99f0386dd9e24e14c5b
DexterFLN <dragon_dexter-x@yahoo.com.br>
2024-07-03 23:10:21 -0300

    test: Correção do Teste Unitário do AuthJwtClaimTest

--- src/test/java/br/com/controle/certo/infrastructure/config/resourceserver/AuthJwtClaimTest.java
+++ src/test/java/br/com/controle/certo/infrastructure/config/resourceserver/AuthJwtClaimTest.java
@@ -1,20 +1,20 @@
 package br.com.controle.certo.infrastructure.config.resourceserver;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Collections;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
+@ExtendWith(MockitoExtension.class)
 public class AuthJwtClaimTest {
 
     @Mock
@@ -23,10 +23,16 @@ public class AuthJwtClaimTest {
     @InjectMocks
     private AuthJwtClaim authJwtClaim;
 
+    @BeforeEach
+    void setUp() {
+        // Reset the security context before each test
+        SecurityContextHolder.clearContext();
+    }
+
     @Test
     void testGetDocumentNumberUserFromJwt() {
         // Mocking JWT
-        Map<String, Object> claims = Collections.singletonMap("document_number", "123456789");
+        Map<String, Object> claims = Collections.singletonMap("user_name", "12345678901");
         Jwt jwt = new Jwt("tokenValue", null, null, claims, claims);
         JwtAuthenticationToken jwtAuthenticationToken = new JwtAuthenticationToken(jwt);
 
@@ -39,6 +45,6 @@ public class AuthJwtClaimTest {
         String documentNumber = authJwtClaim.getDocumentNumberUserFromJwt();
 
         // Asserting the result
-        assertEquals("123456789", documentNumber);
+        assertEquals("12345678901", documentNumber);
     }
 }
